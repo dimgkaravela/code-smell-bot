c1b1ca7d41f0d4f142e5f13a7c0b545ff16f07e6
dimgkaravela <115576836+dimgkaravela@users.noreply.github.com>
2025-10-23 12:59:43 +0300

    Implement PullRequestDiffCollector to summarize PR diffs

---
+++ src/main/java/dev/dimitra/bot/PullRequestDiffCollector.java
@@ -0,0 +1,27 @@
+package dev.yourorg.bot;
+
+import dev.dimitra.bot.model.ChangedFile;
+import dev.dimitra.bot.model.PRDiffSummary;
+
+import java.util.List;
+
+public class PullRequestDiffCollector {
+
+    public PRDiffSummary buildSummary(String repository, int prNumber, List<ChangedFile> allFiles) {
+        PRDiffSummary sum = new PRDiffSummary();
+        sum.repository = repository;
+        sum.prNumber = prNumber;
+        sum.totalFiles = allFiles.size();
+
+        List<ChangedFile> javaFiles = allFiles.stream()
+                .filter(ChangedFile::isJavaFile)
+                .toList();
+
+        sum.javaFiles = javaFiles.size();
+        sum.javaFilesWithPatch = (int) javaFiles.stream().filter(ChangedFile::isTextual).count();
+        sum.totalAdditions = javaFiles.stream().mapToInt(f -> f.additions).sum();
+        sum.totalDeletions = javaFiles.stream().mapToInt(f -> f.deletions).sum();
+        sum.javaChangedFiles = javaFiles;
+        return sum;
+    }
+}
