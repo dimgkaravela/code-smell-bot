d228f863666729eeac3679a127be973d58c3d7ea
dimgkaravela <115576836+dimgkaravela@users.noreply.github.com>
2025-10-30 09:47:57 +0200

    Delete initial test for maven

--- src/main/java/dev/dcg/bot/App.java
+++
@@ -1,7 +0,0 @@
-package dev.dcg.bot;
-public class App {
-  public static void main(String[] args) {
-    System.out.println("Code Smell Bot – skeleton OK");
-    //added a comment here to see if day2 works
-  }
-}
